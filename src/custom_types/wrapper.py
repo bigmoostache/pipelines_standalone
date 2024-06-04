86a0091d56fd540b3bbc1ccf41aec635baf0edec
Guillaume Draznieks <g.draznieks@gmail.com>
2024-06-04 14:36:56 +0000

    Your commit message

--- src/custom_types/wrapper.py
+++ src/custom_types/wrapper.py
@@ -5,9 +5,12 @@ class TYPE:
                  _class,
                  converter,
                  inputable : bool = True,
-                 additional_converters : dict = {}):
+                 additional_converters : dict = {},
+                 visualiser = None
+                 ):
         self.extension = extension
         self.customclass = _class
         self.converter = converter
         self.inputable = inputable
         self.additional_converters = additional_converters
+        self.visualiser = visualiser
