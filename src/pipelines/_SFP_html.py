9055c7e03e1cb37713a4bbbf4d0fde7561cca4f2
Guillaume Draznieks <guillaume.draznieks@blends.fr>
2024-06-10 07:19:21 -0400

    dqw

--- src/pipelines/_SFP_html.py
+++ src/pipelines/_SFP_html.py
@@ -5,7 +5,7 @@ class Pipeline:
     def __init__(self):
         self.converter = Converter
 
-    def __call__(self, metadata : dict) -> bytes:
+    def __call__(self, metadata : dict) -> HTML:
         file_name = metadata['file_name']
         dataset_id = metadata['dataset_id']
         file, = SQL_F1('SELECT "file_contents" FROM "DatasetsFiles" WHERE "dataset_id" = %s AND "file_name" = %s', (dataset_id, file_name))
