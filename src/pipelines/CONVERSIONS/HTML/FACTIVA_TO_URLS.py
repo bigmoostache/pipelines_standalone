6b7d2e81cb19a4c2a8d64a6642c737438fcc603f
Guillaume Draznieks <guillaume.draznieks@blends.fr>
2024-06-10 07:17:00 -0400

    factiva

--- src/pipelines/CONVERSIONS/HTML/FACTIVA_TO_URLS.py
+++ src/pipelines/CONVERSIONS/HTML/FACTIVA_TO_URLS.py
@@ -1,34 +1,34 @@
-from custom_types.URL.type import URL, HTML
+from custom_types.URL.type import URL
+from custom_types.HTML.type import HTML
 from bs4 import BeautifulSoup
 from typing import List
-
-def parse_html(html: str) -> List[URL]:
-    soup = BeautifulSoup(html, 'html.parser')
-    urls = []
-
-    for article in soup.find_all('td', class_='article'):
-        a_tag = article.find('a')
-        if not a_tag:
-            continue
-        
-        url = a_tag['href']
-        title = a_tag.text.strip()
-        text = article.find_next_sibling('div', class_='styleenfulltext').get_text(strip=True)
-        clean_text = ' '.join(text.split())
-        date_meta = article.find_next_sibling('div', class_='styleenfulltextmeta')
-        date = date_meta.find('p', class_='articleMeta').text.split(',')[1].strip() if date_meta else ""
-
-        images = []
-        for img in article.find_all('img'):
-            images.append(img['src'])
-
-        urls.append(URL(url=url, title=title, images=images, success=True, text=text, clean_text=clean_text, date=date))
-
-    return urls
 
 class Pipeline:
 
     def __init__(self):
         pass
     def __call__(self, html : HTML) -> List[URL]:
-        return parse_html(html)
+        html = html.html
+        soup = BeautifulSoup(html, 'html.parser')
+        urls = []
+        for article in soup.find_all('td', class_='article'):
+            a_tag = article.find_all('a')
+            TITLE = ""
+            for x in a_tag:
+                if not x.text.strip():
+                    continue
+                TITLE = x.text.strip()
+                break
+            if not TITLE:
+                continue
+            tags_of_interest = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li', 'blockquote', 'table', 'tr', 'td', 'th', 'ul', 'ol']
+            text_content = []
+            for tag in article.find_all(tags_of_interest):
+                if tag.name.startswith('h'):
+                    text_content.append(f'\n{tag.get_text()}\n')
+                else:
+                    text_content.append(tag.get_text())
+            IMAGES = [x.get('src') for x in article.find_all('img')]
+            CONTENT = ' '.join(text_content).strip()
+            urls.append(URL(url = "localhost", tilte = TITLE, images = IMAGES, success = True, text = CONTENT))
+        return urls
