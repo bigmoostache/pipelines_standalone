8b60610ed6fcbc14f52411e05053032021888561
bigmoostache <g.draznieks@gmail.com>
2024-07-12 17:12:58 +0200

    Update 2_DICTS.py

--- src/pipelines/CONVERSIONS/TXT/2_DICTS.py
+++ src/pipelines/CONVERSIONS/TXT/2_DICTS.py
@@ -6,10 +6,13 @@ class Pipeline:
         pass 
 
     def __call__(self, text : str) -> List[dict]:
-        text = text.replace('\n', ' ').replace('\t', ' ')
-        sub_text = text[text.find('['):text.rfind(']')+1]
-        dic = json.loads(sub_text)
-        assert isinstance(dic, list)
-        for x in dic:
-            assert isinstance(x, dict)
-        return dic
+        try:
+            text = text.replace('\n', ' ').replace('\t', ' ')
+            sub_text = text[text.find('['):text.rfind(']')+1]
+            dic = json.loads(sub_text)
+            assert isinstance(dic, list)
+            for x in dic:
+                assert isinstance(x, dict)
+            return dic
+        except:
+            return []
