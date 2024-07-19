7b30ed3180f05d418332bb11f7256298167f5d1b
Guillaume Draznieks <g.draznieks@gmail.com>
2024-07-19 11:40:10 +0200

    dwqdqw

---
+++ src/sfp/articles/_SFP_xlsx.py
@@ -0,0 +1,13 @@
+from utils.data.file_systems.get_file_system import FS
+import pandas as pd
+from custom_types.XLSX.type import Converter
+
+class Pipeline:
+    def __init__(self):
+        self.converter = Converter
+
+    def __call__(self, metadata : dict) -> pd.DataFrame:
+        file_id = metadata['file_id']
+        file_system = FS(metadata['file_system'])
+        file = file_system.read_bytes(file_id)
+        return self.converter.from_bytes(file)
