c34a93b3ea5aeb6056b8cd2d39052f1e4e9fe8ff
Guillaume Draznieks <guillaume.draznieks@blends.fr>
2024-07-25 17:08:13 -0400

    pdict output from a llm

---
+++ src/pipelines/LLMS_v2/pdict_instance_output.py
@@ -0,0 +1,67 @@
+from typing import Annotated, List, Optional, Union
+from custom_types.PROMPT.type import PROMPT
+from custom_types.PDICT.type import PDICT
+import openai
+from utils.booleans import to_bool
+from pipelines.CONVERSIONS.txt_2_dict import Pipeline as TXT2DICT
+import logging 
+import os
+logging.basicConfig(level=logging.INFO)
+
+class Pipeline:
+    __env__ = ["openai_api_key"]
+
+    def __init__(self, 
+                 model : str = "gpt-4-turbo", 
+                 base_url : str = "https://api.openai.com/v1",
+                 temperature : int =1, 
+                 retries : int =3, 
+                 max_tokens : int =3500, 
+                 top_p : int =1, 
+                 frequency_penalty : float =0, 
+                 presence_penalty : float=0):
+        self.model = model
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
+        self.retries = retries
+        self.base_url = base_url
+        
+    def __call__(self, 
+                 p : PROMPT,
+                 structure : PDICT
+                 ) -> PDICT:
+        
+        for _ in range(self.retries -1):
+            try:
+                return self.retry__call__(p, structure)
+            except:
+                pass
+        raise Exception("Failed to get a valid PDICT")
+    
+    def retry__call__(self, 
+                    p : PROMPT,
+                    structure : PDICT
+                 ) -> dict:
+        api_key = os.environ.get("openai_api_key")
+        client = openai.OpenAI(api_key=api_key, base_url=self.base_url)
+        messages = p.messages
+        response = client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            top_p=self.top_p,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            response_format= {"type": "json_object"}
+        )
+        print(response)
+        print(response.choices)
+        
+        res = response.choices[0].message.content
+        dic = TXT2DICT()(res)
+        structure.verify(dic)
+        return dic
