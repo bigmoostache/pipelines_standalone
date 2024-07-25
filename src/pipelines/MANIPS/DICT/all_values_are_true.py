6c1e5991041b3a8d76ff4d8e3bda855d76eb6883
Guillaume Draznieks <guillaume.draznieks@blends.fr>
2024-07-25 17:32:30 -0400

    qwe

---
+++ src/pipelines/MANIPS/DICT/all_values_are_true.py
@@ -0,0 +1,13 @@
+class Pipeline:
+    def __init__(self,
+                 param_name : str,
+                 ):
+        self.param_name = param_name
+
+
+    def __call__(self, dic : dict) -> dict:
+        val = True 
+        for k,v in dic.items():
+            val = val and isinstance(v, bool) and v
+        dic[self.param_name] = val
+        return val
