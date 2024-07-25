73f85b0914466948842dec68c14636d3a6983fae
bigmoostache <g.draznieks@gmail.com>
2024-07-25 15:16:47 +0200

    Update converter.py

--- src/converter.py
+++ src/converter.py
@@ -15,8 +15,9 @@ from custom_types.FLOAT.type import wraped as FLOATWrapped
 from custom_types.BOOL.type import wraped as BOOLWrapped
 from custom_types.NEWSLETTER.type import wraped as NEWSLETTERWrapped
 from custom_types.RA.type import wraped as RAWrapped
+from custom_types.PDICT.type import wraped as PDICTWrapped
 
-all_types = [RAWrapped, URLWrapped,PDFWrapped,BIBWrapped,HTMLWrapped,PUBMEDWrapped,PROMPTWrapped, XLSXWrapped,JSONWrapped,JSONLWrapped, TXTWrapped, BYTESWrapped, FLOATWrapped, INTWrapped, BOOLWrapped, NEWSLETTERWrapped]
+all_types = [PDICTWrapped, RAWrapped, URLWrapped,PDFWrapped,BIBWrapped,HTMLWrapped,PUBMEDWrapped,PROMPTWrapped, XLSXWrapped,JSONWrapped,JSONLWrapped, TXTWrapped, BYTESWrapped, FLOATWrapped, INTWrapped, BOOLWrapped, NEWSLETTERWrapped]
 _allowed_inputs = {_.extension : list(set([_.extension] + [__.extension for __ in all_types if _.extension in __.additional_converters])) for _ in all_types}
 _ext_to_class = {_.extension:_.customclass for _ in all_types}
 _class_to_ext = {v:k for k,v in _ext_to_class.items()}
