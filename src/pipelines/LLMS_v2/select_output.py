c482845dc0652dade96b25fc65975aa10174486b
Guillaume Draznieks <g.draznieks@gmail.com>
2024-09-09 00:11:51 +0200

    qwe

--- src/pipelines/LLMS_v2/select_output.py
+++ src/pipelines/LLMS_v2/select_output.py
@@ -1,12 +1,13 @@
 from custom_types.PROMPT.type import PROMPT
 from custom_types.SELECT.type import SELECT
 import openai
+from typing import List
 import os 
 
 class Pipeline:
     __env__ = ["openai_api_key"]
     def __init__(self, 
-                 model : str = "gpt-4o", 
+                 model : str = "gpt-4o-2024-08-06", 
                  base_url : str = "https://api.openai.com/v1",
                  temperature : float =1, 
                  top_p : float =1
@@ -16,19 +17,33 @@ class Pipeline:
         self.top_p = top_p
         self.base_url = base_url
         
-    def __call__(self, 
+    def try_call(self, 
                  p : PROMPT,
-                 ) -> SELECT:
+                 e : SELECT
+                 ):
         client = openai.OpenAI(
             api_key=os.environ.get("openai_api_key"), 
             base_url=self.base_url)
+        _type = e.get_model()
         completion = client.beta.chat.completions.parse(
-            model="gpt-4o-2024-08-06",
+            model=self.model,
             messages=p.messages,
             temperature=self.temperature,
             top_p=self.top_p,
-            response_format=SELECT,
+            response_format=_type
         )
         event = completion.choices[0].message.parsed
-        return event
+        if not isinstance(event, _type):
+            return False, None
+        return True, e.model_dump()
+    
+    def __call__(self, 
+             p : PROMPT,
+             e : SELECT
+             ) -> dict:
+        for k in range(3):
+            success, result = self.try_call(p, e)
+            if success:
+                return result
+        raise Exception("Failed to extract the event after 3 attempts")
     
--- src/pipelines/LLMS_v2/select_prepare.py
+++ src/pipelines/LLMS_v2/select_prepare.py
@@ -1,12 +1,12 @@
 from custom_types.PROMPT.type import PROMPT
-from custom_types.SELECT.type import SELECT
+from custom_types.EXTRACTION.type import Entries
 import openai
 import os 
 
 class Pipeline:
     __env__ = ["openai_api_key"]
     def __init__(self, 
-                 model : str = "gpt-4o", 
+                 model : str = "gpt-4o-2024-08-06", 
                  base_url : str = "https://api.openai.com/v1",
                  temperature : float =1, 
                  top_p : float =1
@@ -18,16 +18,16 @@ class Pipeline:
         
     def __call__(self, 
                  p : PROMPT,
-                 ) -> SELECT:
+                 ) -> Entries:
         client = openai.OpenAI(
             api_key=os.environ.get("openai_api_key"), 
             base_url=self.base_url)
         completion = client.beta.chat.completions.parse(
-            model="gpt-4o-2024-08-06",
+            model=self.model,
             messages=p.messages,
             temperature=self.temperature,
             top_p=self.top_p,
-            response_format=SELECT,
+            response_format=Entries,
         )
         event = completion.choices[0].message.parsed
         return event
