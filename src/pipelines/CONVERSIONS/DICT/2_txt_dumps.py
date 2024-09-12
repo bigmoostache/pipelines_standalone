94eff13104d3da051857c1daf9fce99952347211
Guillaume Draznieks <g.draznieks@gmail.com>
2024-09-12 11:54:52 +0200

    qwe

--- src/pipelines/CONVERSIONS/DICT/2_txt_dumps.py
+++ src/pipelines/CONVERSIONS/DICT/2_txt_dumps.py
@@ -4,4 +4,4 @@ class Pipeline:
     def __init__(self):
         pass
     def __call__(self, dic : dict) -> str:
-        return json.dumps(dic, indent = 2, cls=BytesEncoder)
+        return json.dumps(dic, indent = 2, default=lambda v: str(v))
