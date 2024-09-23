9eea5713977405c204ccbf3215f1b4931aedc45a
juansmacbook <jc.lopez@uniandes.edu.co>
2024-09-23 06:50:10 +0200

    new pipeline

---
+++ src/pipelines/TESTS/json_2_str.py
@@ -0,0 +1,26 @@
+
+from typing import Literal, List
+from custom_types.JSON.type import JSON
+import os
+
+import json
+
+
+
+class Pipeline:
+
+    """
+    
+    This pipeline will take a JSON type and dump in int a string for handling of LLMs
+    """
+
+    def __init__(self, dummy_paramerter: str):
+        
+        self.dummy_paramerter = dummy_paramerter
+
+
+
+    def __call__(self,  json_input : JSON) -> str :
+        json_string = json.dumps(json_input)
+        return json_string
+    
