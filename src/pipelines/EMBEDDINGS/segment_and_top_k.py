9245452849caabe794f03a35c6a9309be0a1970e
juansmacbook <jc.lopez@uniandes.edu.co>
2024-09-23 12:44:49 +0200

    New pipeline

--- src/pipelines/EMBEDDINGS/segment_and_top_k.py
+++ src/pipelines/EMBEDDINGS/segment_and_top_k.py
@@ -53,34 +53,3 @@ class Pipeline:
         pre_output = [texts[i] for i in top_k_indices]
         return "\n".join(pre_output)
     
-
-
-
-
-
-
-
-class Pipeline:
-
-    def __init__(self,
-                 ):
-        
-
-    def __call__(self, article :str) -> List[str]:
-        matches = [[m.group(0), (m.start(), m.end() - 1)] for m in re.finditer(r'(?!\\n\\n)([^\n]|\\n(?!\\n))+', article)]
-        chunked_text, split_idx = zip(*matches)
-        new_chunked_text = []
-        new_split_idx = []
-        for i, chunk in enumerate(chunked_text):
-            length = len(chunk)
-            diff = length - self.max_chars
-
-            if diff>0:
-                new_chunked_text.append(chunk[0:self.max_chars])
-                start, end = split_idx[i][0], split_idx[i][1]
-                new_split_idx.append((start, end +1 - diff))
-            else: 
-                new_chunked_text.append(chunk)
-                start, end = split_idx[i][0], split_idx[i][1]
-                new_split_idx.append((start, end+1))
-        return new_chunked_text
