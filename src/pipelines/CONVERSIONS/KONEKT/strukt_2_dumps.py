1ac5fc52255ba7b4a8670896857284b5c82d3db9
Guillaume Draznieks <g.draznieks@gmail.com>
2024-09-24 14:23:03 +0200

    dqw

--- src/pipelines/CONVERSIONS/KONEKT/strukt_2_dumps.py
+++ src/pipelines/CONVERSIONS/KONEKT/strukt_2_dumps.py
@@ -4,4 +4,4 @@ class Pipeline:
     def __init__(self):
         pass        
     def __call__(self, konekt : GenericType) -> str:
-        return konekt.model_json_dumps(indent=2)
+        return konekt.model_dump_json(indent=2)
