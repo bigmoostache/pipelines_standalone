f4a0530a5d586e6163c91f6b0437b63c977a116c
Guillaume Draznieks <g.draznieks@gmail.com>
2024-09-26 01:24:04 +0200

    dw

--- src/pipelines/MANIPS/JSONL/keep_top.py
+++ src/pipelines/MANIPS/JSONL/keep_top.py
@@ -3,11 +3,12 @@ from custom_types.JSONL.type import JSONL
 
 
 class Pipeline:
-    def __init__(self, sort_on: str, keep : int = 10):
+    def __init__(self, sort_on: str, keep : int = 10, reverse : bool = False):
         self.sort_on = sort_on
         self.keep = keep
+        self.reverse = reverse
 
     def __call__(self, jsonl : JSONL) -> JSONL:
         values = jsonl.lines
-        values.sort(key = lambda x : x.get(self.sort_on, 0))
+        values.sort(key = lambda x : x.get(self.sort_on, 0), reverse = self.reverse)
         return JSONL(values[::-1][:self.keep][::-1])
