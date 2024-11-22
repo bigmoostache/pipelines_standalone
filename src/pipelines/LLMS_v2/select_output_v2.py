303d0b522184e4ed1e325ea0918bf9604a173845
Guillaume Draznieks <g.draznieks@gmail.com>
2024-11-22 18:22:01 +0100

    Add Pipeline class for handling prompts and selections with OpenAI API integration

---
+++ src/pipelines/LLMS_v2/select_output_v2.py
@@ -0,0 +1,18 @@
+from custom_types.PROMPT.type import PROMPT
+from custom_types.SELECT.type import SELECT
+import os 
+
+class Pipeline:
+    __env__ = ["openai_api_key"]
+    def __init__(self, 
+                 model : str = "gpt-4o-2024-08-06", 
+                 rerolls: int = 1,
+                 ):
+        self.model = model
+        self.rerolls = rerolls
+        
+    def __call__(self, 
+             p : PROMPT,
+             e : SELECT
+             ) -> dict:
+        return e(p.messages, openai_api_key=os.environ.get("openai_api_key"), model=self.model, rerolls=self.rerolls)
