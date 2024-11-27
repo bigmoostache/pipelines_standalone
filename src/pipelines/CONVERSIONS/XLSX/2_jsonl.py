6cf2b8edf5b25b32f4411242948707b0010ad08a
Guillaume Draznieks <g.draznieks@gmail.com>
2024-11-27 12:09:12 +0100

    Refactor Pipeline call method to accept XLSX object directly and improve sheet handling logic

--- src/pipelines/CONVERSIONS/XLSX/2_jsonl.py
+++ src/pipelines/CONVERSIONS/XLSX/2_jsonl.py
@@ -5,8 +5,8 @@ class Pipeline:
     def __init__(self):
         pass
 
-    def __call__(self, excel : XLSX) -> JSONL:
-        assert len(excel.sheets) == 1, "Only one sheet is allowed"
-        df = list(excel.sheets.values())[0]
+    def __call__(self, df : XLSX) -> JSONL:
+        assert len(df.sheets) == 1, "Only one sheet is allowed"
+        df = list(df.sheets.values())[0]
         list_of_dicts = df.to_dict(orient='records')
         return JSONL(list_of_dicts)
