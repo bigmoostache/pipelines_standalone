27fc83516bdc820fa5e03297cca2a68c24cd5437
Guillaume Draznieks <g.draznieks@gmail.com>
2024-11-29 10:58:56 +0100

    Implement PDF processing pipeline with support for Surya API and fallback to normal extraction

--- src/pipelines/CONVERSIONS/PDF/2_txt.py
+++ src/pipelines/CONVERSIONS/PDF/2_txt.py
@@ -1,14 +1,46 @@
 from custom_types.PDF.type import PDF
 from pypdf import PdfReader
+from typing import Literal
 import io
+import os
+import requests
+import time
 
 class Pipeline:
-    def __init__(self):
-        pass
+    __env__ = ["surya_api_key"]
+    def __init__(self,
+                 method : Literal['normal', 'surya'] = 'normal',
+                 surya_url : str = "https://www.datalab.to/api/v1/marker"
+                 ):
+        self.method = method
+        self.surya_url = surya_url
     def __call__(self, pdf : PDF) -> str:
-        pdf_stream = io.BytesIO(pdf.file_as_bytes)
-        reader = PdfReader(pdf_stream)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
-        return text
+        if self.method == 'surya':
+            form_data = {
+                'file': ('document.pdf', pdf.file_as_bytes, 'application/pdf'),
+                'langs': (None, "English"),
+                "force_ocr": (None, False),
+                "paginate": (None, False)
+            }
+            headers = {"X-Api-Key": os.environ.get("surya_api_key")}
+            response = requests.post(self.surya_url, files=form_data, headers=headers)
+            data = response.json()
+            max_polls = 300
+            check_url = data["request_check_url"]
+
+            for i in range(max_polls):
+                time.sleep(2)
+                response = requests.get(check_url, headers=headers)
+                data = response.json()
+
+                if data["status"] == "complete":
+                    break
+            text = data['markdown']
+            return text
+        else:
+            pdf_stream = io.BytesIO(pdf.file_as_bytes)
+            reader = PdfReader(pdf_stream)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
+            return text
