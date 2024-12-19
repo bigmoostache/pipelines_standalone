96aadff0ae976b0fa6e3b05318ec3c7fd616c458
Guillaume Draznieks <g.draznieks@gmail.com>
2024-12-19 10:23:49 +0100

    Enhance Pipeline class to ensure id columns are included in DataFrame creation and improve handling of empty dictionaries

--- src/pipelines/CUSTOM_TYPES/STRK/dicts_to_xlsx.py
+++ src/pipelines/CUSTOM_TYPES/STRK/dicts_to_xlsx.py
@@ -24,19 +24,42 @@ class Pipeline:
                             _[my_id_column] = i
             # Compute main_df: keys that are not lists
             direct_keys = list(set([k for dic in dics for k,v in dic.items() if not isinstance(v, list)]))
-            # Check that intersection between direct_keys and sheet_keys is empty
-            intersection = set(direct_keys).intersection(sheet_keys)
-            assert len(intersection) == 0, f'intersection between direct_keys and sheet_keys is not empty: {intersection}'
-            # Check that parent_id_col and my_id_column are direct_keys
+
+            # If we have a parent_df, we must ensure parent_id_col is present in main_df
+            if parent_df is not None and parent_id_col not in direct_keys:
+                direct_keys.append(parent_id_col)
+
+            # If we have sheet_keys, we have a my_id_column that must also be present
+            if len(sheet_keys) > 0:
+                my_id_column = f'{name}_id'
+                if my_id_column not in direct_keys:
+                    direct_keys.append(my_id_column)
+
+            # Re-make direct_keys unique
+            direct_keys = list(set(direct_keys))
+
+            # Now perform the assertions
             if parent_df is not None:
                 assert parent_id_col in direct_keys, f'parent_id_col {parent_id_col} not in direct_keys {direct_keys}'
             if len(sheet_keys) > 0:
+                my_id_column = f'{name}_id'
                 assert my_id_column in direct_keys, f'my_id_column {my_id_column} not in direct_keys {direct_keys}'
+
             # Build main_df
-            main_df = pd.DataFrame([{k:v for k,v in d.items() if k in direct_keys} for d in dics])
+            if len(dics) == 0:
+                # If there are no dicts, create an empty DataFrame. Since we must merge on parent_id_col,
+                # ensure the DataFrame has at least that column (if parent_df is not None).
+                if parent_df is not None:
+                    main_df = pd.DataFrame(columns=[parent_id_col])
+                else:
+                    main_df = pd.DataFrame()
+            else:
+                main_df = pd.DataFrame([{k: v for k,v in d.items() if k in direct_keys} for d in dics])
+
             # Merge with parent_df if it exists
             if parent_df is not None:
                 main_df = parent_df.merge(main_df, left_on=parent_id_col, right_on=parent_id_col, how='left', suffixes=('', f'_{name}'))
+
             # Create the result dictionary
             res = {f'{sheet_number}_{name}' : main_df}
             sheet_number += 1
