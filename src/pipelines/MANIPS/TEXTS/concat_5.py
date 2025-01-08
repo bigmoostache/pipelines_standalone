033d0a511f31c97fbaeeff00276dde7b1ef41923
juansmacbook <jc.lopez@uniandes.edu.co>
2025-01-08 17:38:06 -0500

    added concat 5

---
+++ src/pipelines/MANIPS/TEXTS/concat_5.py
@@ -0,0 +1,19 @@
+class Pipeline:
+
+    def __init__(self, 
+                 format : str):
+        self.format = format
+        assert "__text_1__" in self.format
+        assert "__text_2__" in self.format
+        assert "__text_3__" in self.format
+        assert "__text_4__" in self.format
+        assert "__text_5__" in self.format
+
+        
+    def __call__(self, text_1 : str, text_2 : str, text_3 : str, text_4 : str, text_5: str) -> str:
+        res = self.format.replace("__text_1__", text_1)
+        res = res.replace("__text_2__", text_2)
+        res = res.replace("__text_3__", text_3)
+        res = res.replace("__text_4__", text_4)
+        res = res.replace("__text_5__", text_5)
+        return res
