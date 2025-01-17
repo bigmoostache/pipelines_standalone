91ff19f7750146a90d51b89e3ad9dc052343951b
Guillaume Draznieks <g.draznieks@gmail.com>
2025-01-17 14:21:58 +0100

    Add Azure OpenAI pipeline implementation for TINY_BIB processing

---
+++ src/pipelines/LLMS/azure/tiny_bib_output.py
@@ -0,0 +1,42 @@
+from custom_types.PROMPT.type import PROMPT
+from custom_types.GRID.type import GRID
+from custom_types.TINY_BIB.type import TINY_BIB
+from openai import AzureOpenAI
+import logging 
+import os
+logging.basicConfig(level=logging.INFO)
+
+class Pipeline:
+    __env__ = ['AZURE_OPENAI_API_KEY', 'AZURE_OPENAI_ENDPOINT']
+
+    def __init__(self, 
+                 model : str = "gpt-4o", 
+                 temperature : int =1, 
+                 retries : int =3, 
+                 top_p : int =1):
+        self.model = model
+        self.temperature = temperature
+        self.top_p = top_p
+        self.retries = retries
+        
+    def __call__(self, 
+                 p : PROMPT,
+                 ) -> TINY_BIB:
+        client = AzureOpenAI(
+            api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+            api_version="2024-07-01-preview",
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+        )
+        completion = client.beta.chat.completions.parse(
+            model=self.model,
+            messages=p.messages,
+            temperature=self.temperature,
+            top_p=self.top_p,
+            response_format=TINY_BIB,
+        )
+
+        event = completion.choices[0].message.parsed
+        if not isinstance(event, TINY_BIB):
+            raise Exception("Invalid response")
+        return event
+    
