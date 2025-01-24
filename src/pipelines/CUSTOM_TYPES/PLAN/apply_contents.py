970757b7004451de68e1d486313b9404de4f47ed
Guillaume Draznieks <g.draznieks@gmail.com>
2025-01-24 23:30:05 +0100

    Add leading hash management to Pipeline for Plan titles

--- src/pipelines/CUSTOM_TYPES/PLAN/apply_contents.py
+++ src/pipelines/CUSTOM_TYPES/PLAN/apply_contents.py
@@ -1,20 +1,42 @@
 from typing import List
 from custom_types.PLAN.type import Plan
 
+def count_leading_hashes(s):
+    count = 0
+    for char in s:
+        if char == '#':
+            count += 1
+        else:
+            break
+    return count
+
+# Example usage:
+input_string = "###This is a test"
+print("Number of leading #:", count_leading_hashes(input_string))
+
 class Pipeline:
     def __init__(self, 
-                 ):
-        pass
+                 add_lead_hashes : bool = False,
+                ):
+        self.add_lead_hashes = add_lead_hashes
     
     def __call__(self, 
             p : Plan,
             sections : List[Plan]
             ) -> Plan:
         sections = {_.section_id: _ for _ in sections}
-        def process(_plan: Plan):
+        def process(_plan: Plan, depth = 1):
+            if self.add_lead_hashes:
+                n_hashes = count_leading_hashes(_plan.title)
+                if n_hashes > depth:
+                    n_to_remove = n_hashes - depth
+                    _plan.title = _plan.title[n_to_remove:]
+                elif n_hashes < depth:
+                    _plan.title = '#' * (depth - n_hashes) + _plan.title
+                    
             if _plan.section_type != 'leaf':
                 for _ in _plan.contents.subsections:
-                    process(_)
+                    process(_, depth + 1)
             else:
                 if _plan.section_id in sections:
                     _plan.text = sections[_plan.section_id].text
