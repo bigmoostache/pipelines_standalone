f769402b7edf21a30f468b6c9e028c9d23a6d063
Guillaume Draznieks <g.draznieks@gmail.com>
2025-01-28 00:33:37 +0100

    Add PDF comparison and aggregation pipelines for document change analysis

---
+++ src/pipelines/CONVERSIONS/PDF/comparison.py
@@ -0,0 +1,101 @@
+from custom_types.PDF.type import PDF
+from custom_types.JSONL.type import JSONL
+from pypdf import PdfReader
+from typing import Literal
+import io
+import os
+import re
+import json
+import requests
+import time
+from pipelines.MANIPS.TEXTS.segment_uniform import Pipeline as ChunkingPipeline
+import openai, numpy as np
+
+def get_text(pdf: PDF):
+    form_data = {
+        'file': ('document.pdf', pdf.file_as_bytes, 'application/pdf'),
+        'langs': (None, "English"),
+        "force_ocr": (None, False),
+        "paginate": (None, True),
+        'output_format': (None, 'markdown'),
+        "disable_image_extraction": (None, True)
+    }
+    headers = {"X-Api-Key": os.environ.get("surya_api_key")}
+    response = requests.post("https://www.datalab.to/api/v1/marker", files=form_data, headers=headers)
+    data = response.json()
+    max_polls = 300
+    check_url = data["request_check_url"]
+    
+    for i in range(max_polls):
+        time.sleep(2)
+        response = requests.get(check_url, headers=headers)
+        data = response.json()
+    
+        if data["status"] == "complete":
+            break
+    return data['markdown']
+
+def replace_pattern(text):
+    return re.sub(r'\n\n\{(\d+)\}------------------------------------------------', r'__PAGE_\1__', text)
+
+def paginate(chunks: JSONL):
+    current_page = '0'
+    local_index = 0
+    for line in chunks.lines:
+        numbers = re.findall(r"__PAGE_(\d+)__", line['text'])
+        line['text'] = re.sub(r"__PAGE_\d+__", "", line['text'])
+        line['page_start'] = current_page
+        line['local_index'] = local_index
+        local_index += 1
+        next_current_page=str(max([int(current_page)]+[int(_) for _ in numbers]))
+        if next_current_page != current_page:
+            local_index = 0
+        current_page = next_current_page
+        line['page_end'] = current_page
+        
+def get_embeddings_in_chunks(client, texts, model, chunk_size=1024):
+    embeddings = []
+    for i in range(0, len(texts), chunk_size):
+        chunk = texts[i:i + chunk_size]
+        try:
+            response = client.embeddings.create(input=chunk, model=model).data
+        except:
+            response = client.embeddings.create(input=['no data here'] * len(chunk), model=model).data
+        embeddings.extend(response)
+    return np.array([x.embedding for x in embeddings])
+
+class Pipeline:
+    __env__ = ["surya_api_key", "openai_api_key"]
+    def __init__(self):
+        pass
+    def __call__(self, old_pdf : PDF, new_pdf : PDF) -> JSONL:
+        old_pdf = get_text(old_pdf)
+        new_pdf = get_text(new_pdf)
+        # Replacing the pattern
+        old_pdf = replace_pattern(old_pdf)
+        new_pdf = replace_pattern(new_pdf)
+        # Chunking the text
+        old_pdf = ChunkingPipeline(n_chars = 1000)(old_pdf)
+        new_pdf = ChunkingPipeline(n_chars = 1000)(new_pdf)
+        # Paginating the chunks
+        paginate(old_pdf)
+        paginate(new_pdf)
+        # Embedding the chunks
+        client = openai.OpenAI(api_key=os.environ.get("openai_api_key"))
+        model = 'text-embedding-3-large'
+        texts_old_pdf = [_['text'] for _ in old_pdf.lines]
+        texts_new_pdf = [_['text'] for _ in new_pdf.lines]
+        embeddings_old = get_embeddings_in_chunks(client, texts_old_pdf, model)
+        embeddings_new = get_embeddings_in_chunks(client, texts_new_pdf, model)
+        M = embeddings_old.dot(embeddings_new.T)
+        # Crafting the jobs
+        works = []
+        for i in range(len(new_pdf.lines)):
+            old_chunks = np.argsort(M[:, i])[-5:][::-1]
+            works.append(
+                {
+                    **new_pdf.lines[i],
+                    'old': [old_pdf.lines[j] for j in old_chunks]
+                }
+            )
+        return JSONL(works)
---
+++ src/pipelines/CONVERSIONS/PDF/comparison_aggregate.py
@@ -0,0 +1,98 @@
+from custom_types.JSONL.type import JSONL
+import numpy as np
+import os
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from enum import Enum
+from openai import OpenAI
+
+class ChangeType(Enum):
+    ADDITION = 'change: addition'
+    NO_CHANGE = 'change: no change'
+    FORMULATION = 'change: small change in formulation but not in meaning'
+    SMALL = 'change: small change in meaning'
+    SIGNIFICANT = 'change: significant change in meaning'
+
+class ChangeImportance(Enum):
+    NO_CHANGE = 'importance: no change, so not applicable'
+    NONE = 'importance: totally unsignificant'
+    VOCABULARY = 'importance: simple vocabulary update'
+    NOTIFICATION = 'importance: deserves notification'
+    WARNING = 'importance: deserves warning'
+    DANGER = 'importance: major modification deserving attention'
+
+class Change(BaseModel):
+    new_formulation: str = Field(..., description = 'Extract of the new document, transcribed verbatim, but cleaned from parsing artifacts if necessary.')
+    old_formulation: Optional[str] = Field(..., description = 'Older version (cleaned if necessary), optional if not found in other version')
+    old_formulation_chunk_id: Optional[str] = Field(..., description = 'Older version chunk id, example: "Chunk 321"')
+    changes_description: str = Field(..., description = 'Compare the new and the old version and analyse the implications of those changes.')
+    type_of_change: ChangeType = Field(..., description = 'Level at which this text was modified')
+    importance_of_change : ChangeImportance = Field(..., description = 'Importance for the user')
+    CTA: Optional[str] = Field(..., description = 'call to action to the user if needed. make this CTA short and super synthetic')
+
+class Changes(BaseModel):
+    changes: List[Change]
+
+class Pipeline:
+    __env__ = ["openai_api_key"]
+    def __init__(self):
+        pass
+    def __call__(self, work : dict) -> JSONL:
+        old = work['old']
+        old = {_['chunk_id']:_ for _ in old}
+        chunks = '\n\n'.join([f'>>>>>>>>>>>>>>>>>>>>>>>>>>> Chunk {o["chunk_id"]}\n{o["text"]}' for o in work["old"]])
+        prompt = f""">>>
+>>> Below, extracts of the older versions of the document
+>>>
+
+{chunks}
+
+#########################################
+
+>>>
+>>> Below, extract of new document whose changes you should analyse
+>>>
+
+{work['text']}
+
+>>>
+>>> Below, the Instructions
+>>>
+
+Please analyse the changes from the new document, extracting the changes in the provided data structure:
+- new_formulation: the new version (verbatim but cleaned if necessary)
+- old_formulation (optional): older version (same), optional if not found in other version
+- old_formulation_chunk_id (optional): older version chunk id
+- type_of_change: type of change: Addition, No change, change in formulation, small change in meaning, major change in meaning
+- importance_of_change: how important could this change potentially be for the user?
+- CTA (optional): call to action to the user if needed. make this CTA short and super synthetic
+
+"""
+        client = OpenAI(api_key=os.environ.get("openai_api_key"))
+
+        completion = client.beta.chat.completions.parse(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant that NEVER makes any mistakes"},
+                {"role": "user", "content": prompt}
+            ],
+            response_format=Changes,
+        )
+
+        changes = completion.choices[0].message.parsed
+        del work['old']
+        changes = changes.dict()['changes']
+        for i,c in enumerate(changes):
+            c['change_number'] = i
+            c['type_of_change'] = c['type_of_change'].name
+            c['old_formulation_chunk_id'] = (c['old_formulation_chunk_id'] or '').lower().replace('chunk ', '')
+            c['importance_of_change'] = c['importance_of_change'].name
+            if c['old_formulation_chunk_id']:
+                chunk_id = int(c['old_formulation_chunk_id'])
+                chunk = old[chunk_id]
+                c['old_formulation_page_start'] = chunk['page_start']
+                c['old_formulation_page_end'] = chunk['page_end']
+                c['old_formulation_local_index'] = chunk['local_index']
+                c['old_formulation_raw'] = chunk['text']
+            c.update(work)
+        return JSONL(lines=changes)
