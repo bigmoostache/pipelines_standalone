e5293268763bb78b05a3871475921f732241bc88
Guillaume Draznieks <g.draznieks@gmail.com>
2025-01-28 01:02:17 +0100

    Refactor column order in PDF comparison polish pipeline for consistency

--- src/pipelines/CONVERSIONS/PDF/comparison_polish.py
+++ src/pipelines/CONVERSIONS/PDF/comparison_polish.py
@@ -6,7 +6,7 @@ class Pipeline:
     def __init__(self):
         pass
     def __call__(self, concatenated : JSONL) -> JSONL:
-        column_order = ['page_start', 'local_index', 'page_end', 'text', 'type_of_change', 'importance_of_change', 'CTA', 'change_description', 'new_formulation', 'old_formulation', 'old_formulation_page_start', 'old_formulation_local_index', 'old_formulation_page_end']
+        column_order = ['page_start', 'local_index', 'page_end', 'type_of_change', 'importance_of_change', 'CTA', 'changes_description', 'new_formulation', 'old_formulation', 'old_formulation_page_start', 'old_formulation_local_index', 'old_formulation_page_end']
 
         concatenated.lines.sort(key=lambda x: (int(x['page_start']), (x['local_index'])))
         concatenated.lines = [{k:v.get(k, None) for k in column_order} for v in concatenated.lines]
