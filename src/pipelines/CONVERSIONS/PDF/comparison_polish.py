9114b740cc6689f8c82e4c92c3d3ddda8a4013f3
Guillaume Draznieks <g.draznieks@gmail.com>
2025-02-27 14:01:37 +0100

    Refactor column handling in comparison_polish.py to simplify data structure and improve readability

--- src/pipelines/CONVERSIONS/PDF/comparison_polish.py
+++ src/pipelines/CONVERSIONS/PDF/comparison_polish.py
@@ -6,15 +6,13 @@ class Pipeline:
     def __init__(self):
         pass
     def __call__(self, concatenated : JSONL) -> JSONL:
-        column_order = ['page_start', 'local_index', 'page_end', 'type_of_change', 'importance_of_change', 'CTA', 'changes_description', 'new_formulation', 'old_formulation', 'old_formulation_page_start', 'old_formulation_local_index', 'old_formulation_page_end']
+        column_order = ['page_start', 'local_index', 'page_end', 'type_of_change', 'importance_of_change', 'CTA', 'changes_description', 'new_formulation', 'old_formulation']
         concatenated.lines.sort(key=lambda x: (int(x['page_start']), (x['local_index'])))
         concatenated.lines = [{k:v.get(k, None) for k in column_order} for v in concatenated.lines]
         # increase all page numbers by 1 to match the page numbers in the pdf
         for line in concatenated.lines:
             line['page_start'] = str(int(line['page_start']) + 1) if line['page_start'] is not None else None
             line['page_end'] = str(int(line['page_end']) + 1) if line['page_end'] is not None else None
-            line['old_formulation_page_start'] = str(int(line['old_formulation_page_start']) + 1) if line['old_formulation_page_start'] is not None else None
-            line['old_formulation_page_end'] = str(int(line['old_formulation_page_end']) + 1) if line['old_formulation_page_end'] is not None else None
         # rename columns to more human-readable names
         renames = {
             'page_start': 'Page Start',
@@ -26,9 +24,6 @@ class Pipeline:
             'changes_description': 'Changes Description',
             'new_formulation': 'New Formulation',
             'old_formulation': 'Old Formulation',
-            'old_formulation_page_start': 'Old Formulation Page Start',
-            'old_formulation_local_index': 'Old Formulation Chunk in Page',
-            'old_formulation_page_end': 'Old Formulation Page End'
         }
         concatenated.lines = [{renames[k]: v for k, v in line.items()} for line in concatenated.lines]
         return concatenated
