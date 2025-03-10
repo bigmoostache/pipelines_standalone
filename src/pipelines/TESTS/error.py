4c68517b3e663216e697035d7254e79fafea9f1c
Guillaume Draznieks <g.draznieks@gmail.com>
2025-03-10 23:52:04 +0100

    Initialize error_probability in Pipeline class constructor

--- src/pipelines/TESTS/error.py
+++ src/pipelines/TESTS/error.py
@@ -3,7 +3,7 @@ class Pipeline:
     def __init__(self,
                  error_probability : float = 0.5,
                  ):
-        pass 
+        self.error_probability = error_probability
     def __call__(self, input : str) -> str:
         if random() < self.error_probability:
             raise Exception('Random error')
