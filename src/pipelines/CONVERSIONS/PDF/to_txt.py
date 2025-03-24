f8437e19dc30a1ddbe1be34d8d91a3c5e19a8acf
Guillaume Draznieks <g.draznieks@gmail.com>
2025-03-24 10:41:36 +0100

    Add PDF processing pipeline with support for Surya API and error handling

---
+++ src/pipelines/CONVERSIONS/PDF/to_txt.py
@@ -0,0 +1,58 @@
+from custom_types.PDF.type import PDF
+from pypdf import PdfReader
+from typing import Literal
+import io
+import os
+import json
+import requests
+import time
+
+class Pipeline:
+    __env__ = ["surya_api_key"]
+    def __init__(self,
+                 method : Literal['normal', 'surya'] = 'normal',
+                 surya_url : str = "https://www.datalab.to/api/v1/marker",
+                 force_ocr : bool = False
+                 ):
+        self.method = method
+        self.surya_url = surya_url
+        self.force_ocr = force_ocr
+    def call_without_wrap(self, pdf : PDF) -> str:
+        if self.method == 'surya':
+            form_data = {
+                'file': ('document.pdf', pdf.file_as_bytes, 'application/pdf'),
+                'langs': (None, "English"),
+                "force_ocr": (None, self.force_ocr),
+                "paginate": (None, False)
+            }
+            headers = {"X-Api-Key": os.environ.get("surya_api_key")}
+            response = requests.post(self.surya_url, files=form_data, headers=headers)
+            data = response.json()
+            max_polls = 300
+            check_url = data["request_check_url"]
+
+            for i in range(max_polls):
+                time.sleep(2)
+                response = requests.get(check_url, headers=headers)
+                data = response.json()
+
+                if data["status"] == "complete":
+                    break
+            text = data['markdown']
+            return text
+        else:
+            pdf_stream = io.BytesIO(pdf.file_as_bytes)
+            reader = PdfReader(pdf_stream)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
+            return text
+    def __call__(self, pdf : PDF) -> str:
+        try:
+            return self.call_without_wrap(pdf)
+        except Exception as e:
+            if self.method == 'normal':
+                return 'Corrupted Or Locked PDF'
+            else:
+                self.method = 'normal'
+                return self.__call__(pdf)
