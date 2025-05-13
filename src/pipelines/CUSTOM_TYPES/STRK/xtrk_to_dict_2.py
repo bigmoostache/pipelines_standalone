761bda106496ed0d92379e340e70939972082183
Guillaume Draznieks <g.draznieks@gmail.com>
2025-05-13 13:24:35 +0200

    Refactor Pipeline class to include provider parameter and update simple_call and complicated_call methods to use StructuredPipeline and StrPipeline for processing

--- src/pipelines/CUSTOM_TYPES/STRK/xtrk_to_dict_2.py
+++ src/pipelines/CUSTOM_TYPES/STRK/xtrk_to_dict_2.py
@@ -4,6 +4,9 @@ from custom_types.XTRK.type import DataStructure, _create_model
 import tiktoken 
 import json
 import openai
+from pipelines.LLMS.v3.client import Providers
+from pipelines.LLMS.v3.structured import Pipeline as StructuredPipeline
+from pipelines.LLMS.v3.str import Pipeline as StrPipeline
 
 def removenulls(d):
     if isinstance(d, dict):
@@ -14,13 +17,14 @@ def removenulls(d):
         return d
     
 class Pipeline:
-    __env__ = ["openai_api_key"]
     def __init__(self, 
-                 reflexive_model : str = "o1-preview",
-                 formatter_model : str = "gpt-4o",
+                 provider: Providers = "openai",
+                 reflexive_model : Providers = "o1-preview",
+                 formatter_model : Providers = "gpt-4o",
                  max_tokens : int = 32000,
                  simpler_model_if_no_list : bool = True
                  ):
+        self.provider = provider
         self.reflexive_model = reflexive_model
         self.formatter_model = formatter_model
         self.max_tokens = max_tokens
@@ -42,15 +46,14 @@ class Pipeline:
         p = min(1, n_allowed_text_tokens/n_text_tokens*0.9)
         text = text[:int(len(text) * p)]
         client = openai.OpenAI(api_key=os.environ.get("openai_api_key"))
-        formatted_response = client.beta.chat.completions.parse(
+        p = PROMPT()
+        p.add(text, 'user')
+        p.add(prompt, 'user')
+        return StructuredPipeline(
+            provider=self.provider, 
             model=self.formatter_model,
-            messages = [
-                {"role": "user", "content": [{'type': 'text', 'text': text}]}, 
-                {"role": "user", "content": [{'type': 'text', 'text': prompt}]}
-            ],
-            response_format=_create_model('ExtractedData', schema)
-        )
-        return formatted_response.choices[0].message.parsed.model_dump()
+            convert_back_to_dict=True,
+        )(p, _create_model('ExtractedData', schema))
         
     def complicated_call(self, 
                  text : str,
@@ -63,32 +66,27 @@ class Pipeline:
         dumped_schema         = schema.model_dump()
         dumped_schema         = json.dumps(removenulls(dumped_schema), indent = 2)
         prompt                = f'Please extract data from the article above, as a json dictionary, using the schema description below.\n\n{dumped_schema}\n\n- If you miss information, consequences will be absolutely catastrophic.\n- If you make mistakes, consequences will be absolutely catastrophic.\n-> You should be both super precise (NO MISTAKES) AND have a very very high recall (NO MISSING INFO). You are in competition with other models, so be as good as possible.'
-        open('X.txt', 'w').write(text + '\n\n\n' + prompt)
         n_text_tokens, n_prompt_tokens         = len(encoding.encode(text)), len(encoding.encode(prompt))
         n_allowed_text_tokens = self.max_tokens - n_prompt_tokens
         if n_allowed_text_tokens < 1000:
             raise ValueError(f"Text too long: {n_text_tokens} tokens. Maximum allowed text length: {n_allowed_text_tokens} tokens.")
         p = min(1, n_allowed_text_tokens/n_text_tokens*0.9)
         text = text[:int(len(text) * p)]
-        client = openai.OpenAI(api_key=os.environ.get("openai_api_key"))
-        reflexive_response = client.chat.completions.create(
-            model = self.reflexive_model,
-            messages = [
-                {"role": "user", "content": [{'type': 'text', 'text': text}]}, 
-                {"role": "user", "content": [{'type': 'text', 'text': prompt}]}
-            ]
-        )
-        reflexive_response = reflexive_response.choices[0].message.content
-        open('Y.txt', 'w').write(text + '\n\n\n' + prompt + '\n\n\n' +reflexive_response)
-        formatted_response = client.beta.chat.completions.parse(
+        p = PROMPT()
+        p.add(text, 'user')
+        p.add(prompt, 'user')
+        reflexive_response = StrPipeline(
+            provider=self.provider, 
+            model=self.reflexive_model
+        )(p)
+        p = PROMPT()
+        p.add("Fit the provided json in the provided data strucure, VERBATIM.", 'system')
+        p.add(reflexive_response, 'user')
+        return StructuredPipeline(
+            provider=self.provider, 
             model=self.formatter_model,
-            messages=[
-                {"role": "system", "content": "Fit the provided json in the provided data strucure, VERBATIM."},
-                {"role": "user", "content": reflexive_response},
-            ],
-            response_format=_create_model('ExtractedData', schema)
-        )
-        return formatted_response.choices[0].message.parsed.model_dump()
+            convert_back_to_dict=True,
+        )(p, _create_model('ExtractedData', schema))
     
     def __call__(self, 
                  text : str,
