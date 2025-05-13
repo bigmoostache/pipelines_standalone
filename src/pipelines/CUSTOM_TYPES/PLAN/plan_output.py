7b99107f94ed53a96360fafbf5296f9fadad7e69
Guillaume Draznieks <g.draznieks@gmail.com>
2025-05-13 14:40:53 +0200

    Refactor Pipeline class to initialize with provider parameter and streamline processing logic

--- src/pipelines/CUSTOM_TYPES/PLAN/plan_output.py
+++ src/pipelines/CUSTOM_TYPES/PLAN/plan_output.py
@@ -3,9 +3,10 @@ from typing import List
 import os 
 from custom_types.PROMPT.type import PROMPT
 from custom_types.PLAN.type import Plan
-
+from pipelines.LLMS.v3.client import Providers
 from typing import List, Union, Literal
 from pydantic import BaseModel, Field
+from pipelines.LLMS.v3.structured import Pipeline as StructuredPipeline
 
 class Leaf(BaseModel):
     leaf_bullet_points     : List[str] = Field(..., description = 'Bullet points of topics covered. Provide at least 10, or you will fail at this task.')
@@ -23,30 +24,20 @@ class PlanForLLM(BaseModel):
         return Plan.parse_obj(self.dict())
     
 class Pipeline:
-    __env__ = ["openai_api_key"]
     def __init__(self, 
                  model : str = "gpt-4o", 
+                 provider: Providers = "openai",
                  base_url : str = "https://api.openai.com/v1",
                  temperature : float = 1, 
                  top_p : float = 1
                  ):
-        self.model = model
-        self.temperature = temperature
-        self.top_p = top_p
-        self.base_url = base_url
-    
+        self.pipeline = lambda p : StructuredPipeline(
+            provider=provider, 
+            model=model
+        )(p, PlanForLLM).to_plan()
+        
     def __call__(self, 
             p : PROMPT
             ) -> Plan:
         p.truncate()
-        client = openai.OpenAI(
-            api_key=os.environ.get("openai_api_key"), 
-            base_url=self.base_url)
-        completion = client.beta.chat.completions.parse(
-            model=self.model,
-            messages=p.messages,
-            temperature=self.temperature,
-            top_p=self.top_p,
-            response_format=PlanForLLM
-        )
-        return completion.choices[0].message.parsed.to_plan()
+        return self.pipeline(p)
