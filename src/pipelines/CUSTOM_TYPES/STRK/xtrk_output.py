5e18c881897f7ba5113af333e3d3728f02803cd3
Guillaume Draznieks <g.draznieks@gmail.com>
2025-05-13 11:27:14 +0200

    Refactor Pipeline class to initialize with StructuredPipeline and update __call__ method to use structured processing

--- src/pipelines/CUSTOM_TYPES/STRK/xtrk_output.py
+++ src/pipelines/CUSTOM_TYPES/STRK/xtrk_output.py
@@ -1,19 +1,18 @@
-import os, openai
 from custom_types.PROMPT.type import PROMPT
 from custom_types.XTRK.type import DataStructure
+from pipelines.LLMS.v3.client import Providers
+from pipelines.LLMS.v3.structured import Pipeline as StructuredPipeline
 
 class Pipeline:
-    __env__ = ["openai_api_key"]
-    def __init__(self, 
-                 model : str = "gpt-4o-2024-08-06"):
-        self.model = model
+    def __init__(self, ,
+                provider: Providers = "openai",
+                model : str = "gpt-4o-2024-08-06"):
+        self.pipeline = StructuredPipeline(
+            provider=provider, 
+            model=model, 
+            hard_coded_model='xtrk'
+        )
     def __call__(self, 
-                 p : PROMPT
-                 ) -> DataStructure:
-        return openai.OpenAI(
-            api_key=os.environ.get("openai_api_key")
-            ).beta.chat.completions.parse(
-                    model=self.model,
-                    messages=p.messages,
-                    response_format=DataStructure
-            ).choices[0].message.parsed
+                p : PROMPT
+                ) -> DataStructure:
+        return self.pipeline(p, '', 'structured')
