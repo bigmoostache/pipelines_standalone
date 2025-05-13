1765a811bd50aae74c82bc37028d06fe37171387
Guillaume Draznieks <g.draznieks@gmail.com>
2025-05-13 11:10:38 +0200

    Add 'xtrk' to hard_coded_model options and output_formats in Pipeline class

--- src/pipelines/LLMS/v3/structured.py
+++ src/pipelines/LLMS/v3/structured.py
@@ -6,18 +6,20 @@ from pipelines.LLMS.v3.client import Pipeline as ClientPipeline, Providers
 import json
 from custom_types.trees.gap_analysis.type import ResponseType as Tree_Gap_AnalysisResponseType
 from custom_types.TINY_BIB.type import TINY_BIB
+from custom_types.XTRK.type import DataStructure
 
 output_formats = {
     'tree/gap_analysis': Tree_Gap_AnalysisResponseType,
     'select': SELECT,
-    'tiny_bib': TINY_BIB
+    'tiny_bib': TINY_BIB,
+    'xtrk': DataStructure
 }
 
 class Pipeline:
     def __init__(self,
         provider: Providers = "openai",
         model: str = "gpt-4.1",
-        hard_coded_model: Literal['none', 'tree/gap_analysis', 'select', 'tiny_bib'] = 'none',
+        hard_coded_model: Literal['none', 'tree/gap_analysis', 'select', 'tiny_bib', 'xtrk'] = 'none',
         convert_back_to_dict: bool = False
         ):
         self.provider = provider
